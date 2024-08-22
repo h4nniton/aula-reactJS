10faad3bc7a49eccd883ed9f5b157d44c7d00c6a
Emily Crepaldi <emily.ccarvalho13@gmail.com>
2024-08-22 11:51:55 -0300

    aula 07

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,14 @@
+import Item from "./Item"
+
 function List () {
 
-    resturn (
+    return (
         <>
             <h1>Minha lista</h1>
             <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Fiat" ano_lancamento={1964}/>
+                <Item marca="Renault"/>
             </ul>
         </>
     )
